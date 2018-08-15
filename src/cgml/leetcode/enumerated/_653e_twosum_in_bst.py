f02fed6b41b97b80601dc03155a0ec2bebe6fc25
cgml <constantine.gurnov.official@gmail.com>
2018-08-14 18:24:17 -0700

    - 653 two sums with BST input - cool BFS solution

--- src/cgml/leetcode/enumerated/_653e_twosum_in_bst.py
+++ src/cgml/leetcode/enumerated/_653e_twosum_in_bst.py
@@ -15,4 +15,15 @@ class Solution:
         sums = {}
         return self.twosums(root, sums, t)
 
+    def findTargetBFS(self, root, k):
+        if not root: return False
+        bfs, s = [root], set()
+        for i in bfs:
+            if k - i.val in s: return True
+            s.add(i.val)
+            if i.left: bfs.append(i.left)
+            if i.right: bfs.append(i.right)
+        return False
+
+
 assert Solution().findTarget(build_btree([5,3,6,2,4,None,7]),9)

475d9049490a35493020109f602a58ca7f0ae594
cgml <constantine.gurnov.official@gmail.com>
2018-09-06 17:56:16 -0700

    - 673m number of longest increasing sub-sequences

--- src/cgml/leetcode/enumerated/_673m_number_longest_subsequence.py
+++ src/cgml/leetcode/enumerated/_673m_number_longest_subsequence.py
@@ -1,6 +1,7 @@
 class Solution:
     def findNumberOfLIS(self, nums):
         N = len(nums)
+        if N <= 1: return N
         lengths = [0]*N
         counts = [1]*N
         for idx in range(N):
@@ -14,6 +15,8 @@ class Solution:
         maxlength = max(lengths)
         return sum([v for idx, v in enumerate(counts) if lengths[idx] == maxlength])
 
+assert Solution().findNumberOfLIS([]) == 0
+assert Solution().findNumberOfLIS([1]) == 1
 assert Solution().findNumberOfLIS([1, 3, 5, 4, 7]) == 2
 assert Solution().findNumberOfLIS([1, 3, 5, 4, 7, 10]) == 2
 assert Solution().findNumberOfLIS([2, 2, 2, 2, 2]) == 5
